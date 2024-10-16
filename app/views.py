5c27c0eed6cb945e3a6305fdb9a29aba864054da
KiaraLuz <fernandezperezkiara@gmail.com>
2024-10-16 00:02:20 -0500

    propuesta login

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'signin.html') 
+
+def signin(request):
+    return render(request, "signin.html")
